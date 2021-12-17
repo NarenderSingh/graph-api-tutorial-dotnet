6c8908c202f998a0c523504b6f58b79e6e4b9a1e
Narender Singh <narender.singh@agreeya.com>
2021-12-17 17:50:15 +0530

    Create a new Event

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -98,16 +98,16 @@ namespace GraphTutorial.Controllers
         {
             try
             {
-                var userTimeZone = TZConvert.GetTimeZoneInfo(
-                    User.GetUserGraphTimeZone());
+                // var userTimeZone = TZConvert.GetTimeZoneInfo(
+                //     User.GetUserGraphTimeZone());
                 var startOfWeekUtc = CalendarController.GetUtcStartOfWeekInTimeZone(
-                    DateTime.Today, userTimeZone);
+                    DateTime.Today, System.TimeZoneInfo.Local);
 
                 var events = await GetUserWeekCalendar(startOfWeekUtc);
 
                 // Convert UTC start of week to user's time zone for
                 // proper display
-                var startOfWeekInTz = TimeZoneInfo.ConvertTimeFromUtc(startOfWeekUtc, userTimeZone);
+                var startOfWeekInTz = TimeZoneInfo.ConvertTimeFromUtc(startOfWeekUtc, System.TimeZoneInfo.Local);
                 var model = new CalendarViewModel(startOfWeekInTz, events);
 
                 return View(model);
@@ -121,6 +121,91 @@ namespace GraphTutorial.Controllers
 
                 return View(new CalendarViewModel())
                     .WithError("Error getting calendar view", ex.Message);
+            }
+        }
+
+        // Minimum permission scope needed for this view
+        [AuthorizeForScopes(Scopes = new[] { "Calendars.ReadWrite" })]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AuthorizeForScopes(Scopes = new[] { "Calendars.ReadWrite" })]
+        public async Task<IActionResult> New([Bind("Subject,Attendees,Start,End,Body")] NewEvent newEvent)
+        {
+            var timeZone = User.GetUserGraphTimeZone();
+
+            // Create a Graph event with the required fields
+            var graphEvent = new Event
+            {
+                Subject = newEvent.Subject,
+                Start = new DateTimeTimeZone
+                {
+                    DateTime = newEvent.Start.ToString("o"),
+                    // Use the user's time zone
+                    TimeZone = timeZone
+                },
+                End = new DateTimeTimeZone
+                {
+                    DateTime = newEvent.End.ToString("o"),
+                    // Use the user's time zone
+                    TimeZone = timeZone
+                }
+            };
+
+            // Add body if present
+            if (!string.IsNullOrEmpty(newEvent.Body))
+            {
+                graphEvent.Body = new ItemBody
+                {
+                    ContentType = BodyType.Text,
+                    Content = newEvent.Body
+                };
+            }
+
+            // Add attendees if present
+            if (!string.IsNullOrEmpty(newEvent.Attendees))
+            {
+                var attendees =
+                    newEvent.Attendees.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+                if (attendees.Length > 0)
+                {
+                    var attendeeList = new List<Attendee>();
+                    foreach (var attendee in attendees)
+                    {
+                        attendeeList.Add(new Attendee
+                        {
+                            EmailAddress = new EmailAddress
+                            {
+                                Address = attendee
+                            },
+                            Type = AttendeeType.Required
+                        });
+                    }
+
+                    graphEvent.Attendees = attendeeList;
+                }
+            }
+
+            try
+            {
+                // Add the event
+                await _graphClient.Me.Events
+                    .Request()
+                    .AddAsync(graphEvent);
+
+                // Redirect to the calendar view with a success message
+                return RedirectToAction("Index").WithSuccess("Event created");
+            }
+            catch (ServiceException ex)
+            {
+                // Redirect to the calendar view with an error message
+                return RedirectToAction("Index")
+                    .WithError("Error creating event", ex.Error.Message);
             }
         }
     }
