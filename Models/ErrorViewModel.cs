9635c5a30ad64e48be83d417f384e975eb784d32
Narender Singh <narender.singh@agreeya.com>
2021-12-17 12:33:57 +0530

    Created an ASP.NET Core MVC web app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace graph_api_tutorial_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
